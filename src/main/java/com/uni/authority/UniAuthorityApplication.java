89fc29cd25ef9396231df8c99090787f227b3d64
songning <1457065857@qq.com>
2020-04-29 16:55:36 +0800

    first commit

---
+++ src/main/java/com/uni/authority/UniAuthorityApplication.java
@@ -0,0 +1,13 @@
+package com.uni.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniAuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniAuthorityApplication.class, args);
+    }
+
+}

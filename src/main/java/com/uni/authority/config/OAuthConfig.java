f22e8872c681b76cb46e0431a24a9aa41b8be38e
songning123456 <1457065857@qq.com>
2020-05-27 21:57:26 +0800

    切换至Spring Cloud Security

---
+++ src/main/java/com/uni/authority/config/OAuthConfig.java
@@ -0,0 +1,35 @@
+package com.uni.authority.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+/**
+ * @author sonin
+ * @date 2020/5/27 21:53
+ */
+@Configuration
+public class OAuthConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("malldemo")
+                .secret("pgDBd99tOX8d")
+                .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password", "client_credentials")
+                .scopes("webmall");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+    }
+}
